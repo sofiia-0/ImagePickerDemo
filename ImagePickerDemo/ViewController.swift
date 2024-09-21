2f394ecf6157c461a565dd1e7140e0a9760e83bf
sofiia-0 <ana.sofiia02@gmail.com>
2024-09-21 14:40:37 -0600

    Initial Commit

---
+++ ImagePickerDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagePickerDemo
+//
+//  Created by LABORATORIO MAC UAM on 21/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
